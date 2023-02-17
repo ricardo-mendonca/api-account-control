85cfb86fc13d691414ad2840b74509ef6af240de
ricardo-mendonca <ricardo-mendonca@live.com>
2023-02-17 19:57:39 -0300

    Atualização

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,19 +1,15 @@
 import { Router } from 'express';
 import {StatusCodes} from 'http-status-codes';
+import {CidadesController} from './../Controllers';
 
 const router = Router();
 
 
-
-router.get('/', (req, res) => {
+router.get('/', (_, res) => {
     return res.send('Olá Curioso');
 });
 
-router.post('/teste', (req, res) => {
-    console.log(req.body);
-
-    return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
-});
+router.post('/cidades', CidadesController.create);
 
 
 
