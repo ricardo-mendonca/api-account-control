6f137c4a3b56061f7efee1812fb4bbe67eb7016b
ricardo-mendonca <ricardo-mendonca@live.com>
2023-02-20 10:34:30 -0300

    criacao do crud cidades

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -7,6 +7,10 @@ router.get('/', (_, res) => {
     return res.send('Olá Curioso');
 });
 
+router.get('/cidades', CidadesController.getAllValidation, CidadesController.getAll);
+router.get('/cidades/:id', CidadesController.getByIdValidation, CidadesController.getById);
 router.post('/cidades', CidadesController.createValidation, CidadesController.create);
+router.put('/cidades/:id', CidadesController.updateByIdValidation, CidadesController.updateById);
+router.delete('/cidades/:id', CidadesController.deleteByIdValidation, CidadesController.deleteById);
 
 export { router };
