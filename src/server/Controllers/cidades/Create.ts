4bda5ad489115fad278e26a77d53bad5c9771853
ricardo-mendonca <ricardo-mendonca@live.com>
2023-02-20 20:37:45 -0300

    Atualização do projto para Digital Ocean

--- src/server/Controllers/cidades/Create.ts
+++ src/server/Controllers/cidades/Create.ts
@@ -15,7 +15,7 @@ export const createValidation = validation((getSchema) => ({
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
     console.log(req.body);
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('OPS! Não implementado');
+    return res.status(StatusCodes.CREATED).json(12);
 };
 
 /* exemplo de IFilter
