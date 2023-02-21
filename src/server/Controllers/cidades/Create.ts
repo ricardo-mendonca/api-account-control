f73acb14e9f7dc782d97f35b2dac3ae078e1ec70
ricardo-mendonca <ricardo-mendonca@live.com>
2023-02-21 10:05:16 -0300

    Atualização models

--- src/server/Controllers/cidades/Create.ts
+++ src/server/Controllers/cidades/Create.ts
@@ -1,14 +1,13 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import * as yup from 'yup';
+import { ICidade } from '../../database/models';
 import { validation } from '../../shared/middleware';
 
-interface ICidade {
-    nome: string;
-}
+interface IBodyProps extends Omit<ICidade,'id'> {}
 
 export const createValidation = validation((getSchema) => ({
-    body: getSchema<ICidade>(yup.object().shape({
+    body: getSchema<IBodyProps>(yup.object().shape({
         nome: yup.string().required().min(3),
     })),
 }));
