443b17ac59b73018e3310a5aa0d7eec411d315da
ricardo-mendonca <ricardo-mendonca@live.com>
2023-02-25 08:32:41 -0300

    Criando providers de pessoas

---
+++ src/server/database/providers/pessoas/GetAll.ts
@@ -0,0 +1,20 @@
+import { ETableNames } from '../../ETableNames';
+import { IPessoa } from '../../models';
+import { Knex } from '../../knex';
+
+
+export const getAll = async (page: number, limit: number, filter: string): Promise<IPessoa[] | Error> => {
+    try {
+        const result = await Knex(ETableNames.pessoa)
+            .select('*')
+            .where('nome', 'like', `%${filter}%`)
+            .where('sobrenome', 'like', `%${filter}%`)
+            .offset((page - 1) * limit)
+            .limit(limit);
+
+        return result;
+    } catch (error) {
+        console.log(error);
+        return new Error('Erro ao consultar os registros');
+    }
+};
