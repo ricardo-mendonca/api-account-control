e0b00cf6b5179c1c4a94afcd24760db8f37702b9
ricardo-mendonca <ricardo-mendonca@live.com>
2023-02-27 16:19:37 -0300

    Atualização CORS

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,12 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
-import './shared/services/TranslationsYup';
-import {router} from './routes';
 
+import './shared/services/TranslationsYup';
+import { router } from './routes';
 
 const server = express();
 
+
+server.use(cors());
+
 server.use(express.json());
+
 server.use(router);
 
 export { server };
